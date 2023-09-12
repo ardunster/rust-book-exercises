612a4200c46b5e2881141141f59f47175c7e778f
Anna Dunster <anna@annardunster.com>
2023-09-12 16:05:28 -0700

    Fun with variables

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -1,3 +1,10 @@
 fn main() {
-    println!("Hello, world!");
+    let mut xavier = 5;
+    println!("value of x: {xavier}");
+    xavier = 6;
+    println!("value of x: {xavier}");
+
+    const THREE_HOURS_IN_SECONDS: u32 = 60 * 60 * 3;
+
+    println!("It's very important you know. Three hours worth of seconds: {THREE_HOURS_IN_SECONDS}")
 }
