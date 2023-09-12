577fda4f795e3285c431538f9e962607b457b101
Anna Dunster <anna@annardunster.com>
2023-09-12 16:08:29 -0700

    Variable shadowing and scope

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -6,5 +6,16 @@ fn main() {
 
     const THREE_HOURS_IN_SECONDS: u32 = 60 * 60 * 3;
 
-    println!("It's very important you know. Three hours worth of seconds: {THREE_HOURS_IN_SECONDS}")
+    println!("It's very important you know. Three hours worth of seconds: {THREE_HOURS_IN_SECONDS}");
+
+    let xandrew = 12;
+
+    let xandrew = xandrew + 4;
+
+    {
+        let xandrew = xandrew * 3;
+        println!("Xandrew in the inner scope: {xandrew}");
+    }
+
+    println!("Xandrew in the outer scope: {xandrew}")
 }
