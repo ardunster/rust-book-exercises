27add8aeb368aaedb93d88c90cc59dc464b62b5d
Anna Dunster <anna@annardunster.com>
2023-09-15 14:50:22 -0700

    fun with branching

--- branching/src/main.rs
+++ branching/src/main.rs
@@ -1,3 +1,18 @@
 fn main() {
     println!("Hello, world!");
+
+    let x = 12;
+
+    if x % 4 == 0 {
+        println!("divisible by 4")
+    } else if x < 17 {
+        println!("less than 17");
+    } else {
+        println!("greater than 17");
+    }
+
+    let want_pizza = true;
+    let food_to_get = if want_pizza { "pizza" } else { "borgir" };
+
+    println!("We're getting {food_to_get} today!")
 }
