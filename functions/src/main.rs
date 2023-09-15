ca701a1e6345009a31913b9a9b228ca60db56391
Anna Dunster <anna@annardunster.com>
2023-09-15 14:32:38 -0700

    Fun with functions

--- functions/src/main.rs
+++ functions/src/main.rs
@@ -1,9 +1,37 @@
 fn main() {
-    println!("Hello, world!");
+    println!("Hello, {{world!}}");
 
-    some_other_thing();
+    some_other_thing(7, ":O !!!!! ");
+
+    let thing = returns_a_thing();
+
+    println!("thing is: {thing}");
+
+    println!("not return anything is: {:?}", not_return_anything());
+
+    println!("add 42 to a thing: {}", add_forty_two(thing));
 }
 
-fn some_other_thing() {
-    println!("steve");
+fn some_other_thing(input: i32, exclamation: &str) {
+    println!("steve likes the number {input} {exclamation}");
+
+    let steve = {
+        let x = 1;
+        // Note no semicolon here!
+        x + 3
+    };
+
+    println!("Steve has added up to {steve}");
+}
+
+fn returns_a_thing() -> i32 {
+    6
+}
+
+fn not_return_anything() {
+    6;
+}
+
+fn add_forty_two(input: i32) -> i32 {
+    input + 42
 }
